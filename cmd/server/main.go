25efff516e259fc0bc6e154acd8092787831a943
Keuangan Developer <dev@trueliability.com>
2026-03-27 00:34:14 +0700

    chore(api): enable CORS for flutter connectivity

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -11,6 +11,7 @@ import (
 	"keuangan_backend/internal/router"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/cors"
 	"github.com/gofiber/fiber/v3/middleware/logger"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/joho/godotenv"
@@ -65,6 +66,11 @@ func main() {
 	})
 
 	// Middleware
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: []string{"*"},
+		AllowHeaders: []string{"Origin, Content-Type, Accept, Authorization"},
+		AllowMethods: []string{"GET, POST, PUT, DELETE, OPTIONS"},
+	}))
 	app.Use(logger.New())
 
 	// Routes
